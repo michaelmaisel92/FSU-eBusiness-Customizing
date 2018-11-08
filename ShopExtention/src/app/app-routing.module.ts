74cdeb319254b46bde913685174be63edf91ce74
michaelmaisel <kontakt@michaelmaisel.de>
2018-11-08 16:35:09 +0100

    Creating routing module for Products component; Setting routing (https://domain.de/products)

--- ShopExtention/src/app/app-routing.module.ts
+++ ShopExtention/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'products', component: ProductListComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
