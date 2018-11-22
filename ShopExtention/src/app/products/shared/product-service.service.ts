e34537b87250409c1eca08e65a17cf34d115c0b6
michaelmaisel <kontakt@michaelmaisel.de>
2018-11-22 23:32:05 +0100

    implementing image functionality

--- ShopExtention/src/app/products/shared/product-service.service.ts
+++ ShopExtention/src/app/products/shared/product-service.service.ts
@@ -17,7 +17,9 @@ export class ProductServiceService {
   }
 
   url = "http://prestashopa.netglue.de/api";
+  // This needs to be more secure
   key = "LL11XQ47LUKNVVLQ3MY31UUW4UATBE4Q";
+  // Get output in JSON instead of XML
   url_Addons = "output_format=JSON&display=full";
 
   buildUrl() {
@@ -29,15 +31,28 @@ export class ProductServiceService {
     console.log("Getting JSON from REST API");
     this.result = this.httpClient.get(this.buildUrl());
     this.result.subscribe(data => {
+      console.log(data);
       let numberOfProducts = data.products.length;
       for (let index = 0; index < numberOfProducts; index++) {
 
         var product = <Product>{};
+
+        product.id = data.products[index].id;
+
         product.name = data.products[index].name[0].value;
-        product.price = data.products[index].price;
+
+        // Write method to build a proper price string
+        product.price = data.products[index].price.substring(0, data.products[index].price.length - 4) + ' €';
+
         product.availablilty = data.products[index].available_for_order;
-        product.description = data.products[index].description_short[0].value;
+
+        // Write method to remove unnecessary html-tags
+        product.description = data.products[index].description_short[0].value.replace('<p>','').replace('</p>','');
+
         product.forsale = data.products[index].on_sale;
+
+        // Set image url while using getProductImage method
+        product.imageurl = this.getProductImage(product.id);
 
         this.productsArray.push(product);
 
@@ -47,6 +62,13 @@ export class ProductServiceService {
       console.log(error);
     })
     return this.productsArray;
+  }
+
+  getProductImage(productid): string{
+      // IMPLEMENT THIS ;)
+    let url = 'http://prestashopa.netglue.de/api/images/products/' + productid + imageid + '?ws_key=' + this.key;
+    console.log(url);
+    return url;
   }
 
 
--- ShopExtention/src/app/products/shared/product.interface.ts
+++ ShopExtention/src/app/products/shared/product.interface.ts
@@ -1,7 +1,9 @@
 export interface Product {
+    id: number;
     name: string;
     price: string;
     availablilty: string;
     forsale: string;
     description: string;
+    imageurl: string;
 }
