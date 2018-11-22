32bb31a7fe55291c277b143bcd0f75be35a22bfa
Max <nessenmaxi@googlemail.com>
2018-11-22 20:07:56 +0100

    parse array structure from respone

--- ShopExtention/src/app/products/product-list/product-list.component.ts
+++ ShopExtention/src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductServiceService } from '../shared/product-service.service';
 import { Product } from '../shared/product.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-product-list',
@@ -11,12 +13,51 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
 
-  constructor(private ps: ProductServiceService) { }
+  result:Observable<any>;
+
+  constructor(private ps: ProductServiceService, private http : HttpClient) { }
+
+
+  //URL
+  url = "http://prestashopa.netglue.de/api";
+  key = "LL11XQ47LUKNVVLQ3MY31UUW4UATBE4Q";
+  url_Addons = "output_format=JSON&display=full";
+
+  //Requests
+  categorie = "categories";
+  product = "products";
+
+  //Array Buffer
+  bufferProduct: any = [];
+  bufferCategory = [];
+  
+
+  buildUrl(type){
+    return this.url + '/' + type + '?ws_key=' + this.key+ '&'+ this.url_Addons;
+  } 
+
+  getRequest(type){
+    this.result = this.http.get(this.buildUrl(type));
+      this.result.subscribe( data => {
+        console.log( data );
+        this.bufferProduct =  data ;
+        if (type == this.product) { 
+          console.log("Name: "+data.products[0].name[0].value);
+          console.log("Price: "+data.products[0].price);
+          console.log("Available: "+data.products[0].available_for_order);
+          console.log("Description: "+data.products[0].description_short[0].value);
+          console.log("Sale: "+data.products[0].on_sale);
+        } else {
+          console.log(data.categories[0].name[0].value); 
+        }
+      }, error => {
+        console.log( error );
+      })
+  }
 
   ngOnInit() {
     this.products = this.ps.getStaticProducts();
-    this.ps.getRequest(this.ps.product);
-    this.ps.getRequest(this.ps.categorie);
+    this.getRequest(this.product);
   }
 
 }
--- ShopExtention/src/app/products/shared/product-service.service.ts
+++ ShopExtention/src/app/products/shared/product-service.service.ts
@@ -1,33 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product.interface';
-import { Observable } from 'rxjs/internal/Observable';
-import { HttpClient } from '@angular/common/http';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductServiceService {
 
-  products: Product[];
-  result:Observable<any>;
+ 
 
-  constructor(private httpClient: HttpClient) {
+  constructor() {
 
    }
-
-  //URL
-  url = "http://prestashopa.netglue.de/api";
-  key = "LL11XQ47LUKNVVLQ3MY31UUW4UATBE4Q";
-  url_Addons = "output_format=JSON&display=full";
-
-  //Requests
-  categorie = "categories";
-  product = "products";
-
-  //Array Buffer
-  Products = [];
-  Categories = [];
 
 
    getStaticProducts(): Product[] {
@@ -57,33 +40,4 @@ export class ProductServiceService {
        }
      ];
    }
-
-   getDynamicProducts(): Product[] {
-     // Call API here and get Products dynamically, depending on given URL parameters
-    return [];
-  }
-
-  buildUrl(type){
-    return this.url + '/' + type + '?ws_key=' + this.key+ '&'+ this.url_Addons;
-  } 
-
-  getRequest(type){
-    this.result = this.httpClient.get(this.buildUrl(type));
-      this.result.subscribe( data => {
-        console.log( data );
-        if (type == this.product) {
-          this.Products = data; 
-          console.log(this.Products);
-          console.log(data.products[0].name[0].value);
-        } else {
-          this.Categories = data;
-          console.log(this.Categories);
-          console.log(data.categories[0].name[0].value); 
-        }
-      }, error => {
-        console.log( error );
-      })
-  }
-
-
 }
